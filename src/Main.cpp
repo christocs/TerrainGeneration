88431fe2017fcaf825d562450abf2c8627e60024
Christo Stephenson <christo@blueskylabs.com.au>
2020-04-04 13:12:11 +0800

    Remove SFML code

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,20 +1,3 @@
-#include <SFML/Window.hpp>
-#include <iostream>
-
-auto main() -> int {
-    auto window = sf::Window{sf::VideoMode(800, 600), "cmake-example"};
-
-    while (window.isOpen()) {
-        auto event = sf::Event{};
-
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
-    }
-}
-
 #include <iostream>
 
 int main()
